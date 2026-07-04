d049c961dc240de24888c4a8ce37b4c7822a6421
Onyx_i7 <Crackdroid69@gmail.com>
2026-07-04 19:52:22 -0400

    Remove cdq.j calls in TextureAnimationTransformer
    Refactor method to remove calls to cdq.j and log warnings.

--- src/main/java/com/onyxi7/noanimtextures/core/TextureAnimationTransformer.java
+++ src/main/java/com/onyxi7/noanimtextures/core/TextureAnimationTransformer.java
@@ -22,63 +22,25 @@ public class TextureAnimationTransformer implements IClassTransformer {
         ClassNode classNode = new ClassNode();
         classReader.accept(classNode, 0);
         
-        System.out.println("[NoAnimTextures] Analyzing ALL methods in TextureMap...");
-        
-        // Buscar TODOS los métodos que llaman a TextureAtlasSprite
         for (MethodNode method : classNode.methods) {
-            boolean callsSpriteMethod = false;
-            String calledMethod = "";
-            
             for (AbstractInsnNode insn = method.instructions.getFirst(); insn != null; insn = insn.getNext()) {
                 if (insn.getType() == AbstractInsnNode.METHOD_INSN) {
                     MethodInsnNode methodInsn = (MethodInsnNode) insn;
                     
-                    // Buscar cualquier llamada a TextureAtlasSprite (nombre SRG o normal)
-                    if (methodInsn.owner.equals("net/minecraft/client/renderer/texture/TextureAtlasSprite") ||
-                        methodInsn.owner.equals("cdq") ||
-                        methodInsn.owner.equals("bua")) {
+                    if (methodInsn.owner.equals("cdq") && methodInsn.name.equals("j") && methodInsn.desc.equals("()V")) {
+                        System.out.println("[NoAnimTextures] Found cdq.j call in method " + method.name + " - removing it!");
+                        method.instructions.remove(insn);
                         
-                        System.out.println("[NoAnimTextures] Method " + method.name + " calls " + methodInsn.owner + "." + methodInsn.name);
-                        callsSpriteMethod = true;
-                        calledMethod = methodInsn.name;
+                        ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+                        classNode.accept(classWriter);
+                        System.out.println("[NoAnimTextures] Transformation complete!");
+                        return classWriter.toByteArray();
                     }
-                }
-            }
-            
-            // Si este método llama a un método void de TextureAtlasSprite, eliminar esa llamada
-            if (callsSpriteMethod && calledMethod.startsWith("func_") && calledMethod.endsWith("_l")) {
-                System.out.println("[NoAnimTextures] Found method with sprite call: " + method.name + " -> " + calledMethod);
-                
-                int removedCalls = 0;
-                AbstractInsnNode insn = method.instructions.getFirst();
-                while (insn != null) {
-                    AbstractInsnNode next = insn.getNext();
-                    
-                    if (insn.getType() == AbstractInsnNode.METHOD_INSN) {
-                        MethodInsnNode methodInsn = (MethodInsnNode) insn;
-                        if ((methodInsn.owner.equals("net/minecraft/client/renderer/texture/TextureAtlasSprite") ||
-                             methodInsn.owner.equals("cdq") ||
-                             methodInsn.owner.equals("bua")) &&
-                            methodInsn.name.equals(calledMethod) &&
-                            methodInsn.desc.equals("()V")) {
-                            
-                            System.out.println("[NoAnimTextures] Removing call to " + methodInsn.name);
-                            method.instructions.remove(insn);
-                            removedCalls++;
-                        }
-                    }
-                    insn = next;
-                }
-                
-                if (removedCalls > 0) {
-                    System.out.println("[NoAnimTextures] Removed " + removedCalls + " calls!");
                 }
             }
         }
         
-        ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_MAXS);
-        classNode.accept(classWriter);
-        System.out.println("[NoAnimTextures] Transformation complete!");
-        return classWriter.toByteArray();
+        System.out.println("[NoAnimTextures] WARNING: cdq.j not found!");
+        return basicClass;
     }
 }
